30be88bae999f504a50732c3d8f17acc0ba730cd
Akash Kundu <akashkundu1265@gmail.com>
2025-07-26 12:29:33 +0530

    wrote aggregation pipeline

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -11,4 +11,4 @@ const subscriptionSchema = new Schema({
     }
 },{timestamps : true})
 
-export const Subscription = mongoose.model("Subscripttion",subscriptionSchema)
+export const Subscription = mongoose.model("Subscription", subscriptionSchema);
